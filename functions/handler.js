87567df34bc93d43c7aee4538ca49246cef99d33
Robert Reinhard <robert.reinhard@bukwild.com>
2019-01-25 16:54:06 -0800

    Adding missing Sentry setup

--- functions/handler.js
+++ functions/handler.js
@@ -1,12 +1,17 @@
 // Boot Coffeescript
 require('coffeescript/register')
 
+// Raven wrapping helper helper
+var Raven = require('raven'),
+	RavenLambdaWrapper = require('serverless-sentry-lib'),
+	wrap = function(cb) { return RavenLambdaWrapper.handler(Raven, cb) }
+
 // Expose handlers
 module.exports = {
-	listAsanaProjects: require('./handlers/list-asana-projects/index.coffee'),
-	listGitlabProjects: require('./handlers/list-gitlab-projects/index.coffee'),
-	onAsanaWebhook: require('./handlers/on-asana-webhook/index.coffee'),
-	onContentfulWebhook: require('./handlers/on-contentful-webhook/index.coffee'),
-	onGitlabWebhook: require('./handlers/on-gitlab-webhook/index.coffee'),
-	onSlackRequest: require('./handlers/on-slack-request/index.coffee')
+	listAsanaProjects: wrap(require('./handlers/list-asana-projects/index.coffee')),
+	listGitlabProjects: wrap(require('./handlers/list-gitlab-projects/index.coffee')),
+	onAsanaWebhook: wrap(require('./handlers/on-asana-webhook/index.coffee')),
+	onContentfulWebhook: wrap(require('./handlers/on-contentful-webhook/index.coffee')),
+	onGitlabWebhook: wrap(require('./handlers/on-gitlab-webhook/index.coffee')),
+	onSlackRequest: wrap(require('./handlers/on-slack-request/index.coffee'))
 }
